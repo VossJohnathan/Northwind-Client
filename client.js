a2ee646fe1ead010303d39328b1d7da56acee4d4
Johnathan Voss <JVoss11@my.wctc.edu>
2023-08-31 19:57:38 -0500

    Assignment 2 changes

---
+++ client.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(document).keyup(function(e) {
+        if(e.which == 27) {
+            $('#element').toast('hide')
+        }
+    });
+    // preload audio
+    var toast = new Audio('toast.wav');
+    $('.code').on('click', function(e) {
+        //display selected product info
+        const name = $(this).data('product');
+        const code = $(this).data('code'); //code in place of product to show discount code.
+        $('#name').html(name);
+        $('#code').html(code);
+        
+        e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#liveToast').toast({ autohide: false }).toast('show');
+    });
+});
